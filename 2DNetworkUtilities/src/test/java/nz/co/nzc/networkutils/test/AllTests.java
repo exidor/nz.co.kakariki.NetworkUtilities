1abf264c73189dcd203c477ef7cafb58ca2bbf6e
Joseph Ramsay <joseph.ramsay@2degreesmobile.co.nz>
2011-09-15 08:43:10 +1200

    15/9/11

--- 2DNetworkUtilities/src/test/java/nz/co/nzc/networkutils/test/AllTests.java
+++ 2DNetworkUtilities/src/test/java/nz/co/nzc/networkutils/test/AllTests.java
@@ -1,9 +1,5 @@
 package nz.co.nzc.networkutils.test;
 
-
-
-import nz.co.nzc.networkutils.test.BuildNetworkTest;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -12,20 +8,4 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ BuildNetworkTest.class })
 public class AllTests {
 	
-	/*
-	public static Test suite() {
-		TestSuite suite = new TestSuite();
-		suite.addTestSuite(BuildNetworkTest.class);
-
-		return suite;
-	}
-	*/
-
-	/**
-	 * Runs the test suite using the textual runner.
-	 */
-	/*
-	public static void main(String[] args) {
-		junit.textui.TestRunner.run(suite());
-	}*/
 }
